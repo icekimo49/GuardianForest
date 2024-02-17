c9047ffd41cbbcf8364b2dee0483db5951b235d9
RayyanGanteng <hermantorayyan@gmail.com>
2024-02-17 10:36:14 +0700

    bot lari

--- scene/penebang/Penebang_kayu.gd
+++ scene/penebang/Penebang_kayu.gd
@@ -1,23 +1,18 @@
 extends CharacterBody2D
 
-const maksKecepatan = 100
+var speed = 1000
+var kabur = false
+var player = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(delta):
+	if kabur:
+		var start_position = position
+		var target_position = Vector2(1000, 0)  # Ubah posisi tujuan sesuai kebutuhan Anda
+		var direction = (target_position - start_position).normalized()
+		position += direction * speed * delta
+	
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var arah = arah_kePlayer()
-	velocity = arah * maksKecepatan
-	move_and_slide()
-
-
-func arah_kePlayer():
-	var player_nodes = get_tree().get_first_node_in_group("player") as Node2D
-	if (player_nodes != null):
-		return (player_nodes.global_position - global_position).normalized()
-		
-	return Vector2.ZERO
-
+func _on_area_2d_body_entered(body):
+	player = body
+	kabur = true
