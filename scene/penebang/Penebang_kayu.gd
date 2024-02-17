cdb3cbe5595b035f6290310ff6c52ed7c6eda14b
icekimo49 <muhammad.haikal494972@gmail.com>
2024-02-17 14:54:45 +0700

    Update

--- scene/penebang/Penebang_kayu.gd
+++ scene/penebang/Penebang_kayu.gd
@@ -1,17 +1,30 @@
 extends CharacterBody2D
 
+
+
+
 var speed = 1000
 var kabur = false
 var player = null
+var arahRandom = randf()
+
+
 
 func _physics_process(delta):
+	
 	if kabur:
 		var start_position = position
-		var target_position = Vector2(1000, 0)  # Ubah posisi tujuan sesuai kebutuhan Anda
+		var target_position = Vector2(1000,0 )  # Ubah posisi tujuan sesuai kebutuhan Anda
 		var direction = (target_position - start_position).normalized()
 		position += direction * speed * delta
 	
 	
+func pembuatRandom() -> float :
+	var x = 1000
+	if randf() < 0.5:
+		x = -1000
+	return x
+
 
 func _on_area_2d_body_entered(body):
 	player = body
