b00ec1d627a2694f729dc79211ff9d6f958165ba
RayyanGanteng <hermantorayyan@gmail.com>
2024-02-17 13:19:24 +0700

    pohon selesai

--- scene/pohon.gd
+++ scene/pohon.gd
@@ -5,26 +5,31 @@ var posisi = position
 var deteksi = false
 var kebakar = null
 var timer: Timer
+var timer2: Timer
 
 func _on_area_2d_body_entered(body):
 	if deteksi == false:
-		mulai_timer()
-		print("2")
+		mulai_timer1()
 		deteksi = true
 
 func _ready():
 	timer = $Timer
+	timer2 = $Timer2
 	
-func mulai_timer():
+func mulai_timer1():
 	timer.start()
 	
 func _on_timer_timeout():
 	var instance = api.instantiate()
 	instance.position = posisi
 	add_child(instance)
-	
-	
+	mulai_timer2()
 
+func mulai_timer2():
+	timer2.start()
 
 func _on_area_2d_body_exited(body):
-	deteksi = false
+	timer2.stop()
+
+func _on_timer_2_timeout():
+	queue_free()
