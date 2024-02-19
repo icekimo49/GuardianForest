ec721588c1fb4cdaa31722ace47c2cfde97fa541
RayyanGanteng <hermantorayyan@gmail.com>
2024-02-19 08:47:31 +0700

    fix pohon

--- scene/pohon.gd
+++ scene/pohon.gd
@@ -6,7 +6,6 @@ var deteksi = false
 var kebakar = null
 var timer: Timer
 var timer2: Timer
-var timer3: Timer
 
 func _on_area_2d_body_entered(body):
 	if deteksi == false:
@@ -16,7 +15,6 @@ func _on_area_2d_body_entered(body):
 func _ready():
 	timer = $Timer
 	timer2 = $Timer2
-	timer3 = $Timer3
 	
 func mulai_timer1():
 	timer.start()
@@ -30,20 +28,8 @@ func _on_timer_timeout():
 func mulai_timer2():
 	timer2.start()
 
-func mulai_timer3():
-	timer3.start()
-
 func _on_area_2d_body_exited(body):
-	timer2.stop()
+	pass
 
 func _on_timer_2_timeout():
 	queue_free()
-	
-func _on_area_2d_2_body_entered(body):
-	mulai_timer3()
-
-func _on_timer_3_timeout():
-	queue_free()
-
-func _on_area_2d_2_body_exited(body):
-	timer3.stop()
