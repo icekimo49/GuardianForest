1c90362029785f3010b118d31eb75c0eb705a2d0
RayyanGanteng <hermantorayyan@gmail.com>
2024-03-01 00:49:41 +0700

    fix siram 4 arah, api bisa ilang setelah disiram

---
+++ scene/player/player_hitbox_atas.gd
@@ -0,0 +1,9 @@
+extends CollisionShape2D
+
+func _ready():
+	# Menonaktifkan CollisionShape2D saat permainan dimulai
+	disabled = true
+
+# Fungsi untuk mengaktifkan atau menonaktifkan CollisionShape2D
+func toggle_collision(aktif: bool):
+	disabled = !aktif
---
+++ scene/player/sisa_air.gd
@@ -0,0 +1,5 @@
+extends CanvasLayer
+@onready var label = $MarginContainer/TextLabel
+
+func _process(delta):
+	label.text = "memek"
