e52dd38ba9f41dac476002e1da4bb0b94703ea87
icekimo49 <muhammad.haikal494972@gmail.com>
2024-03-01 11:48:35 +0700

    Night Cycle

---
+++ scene/main/NightCycle.gd
@@ -0,0 +1,12 @@
+extends CanvasModulate
+
+
+const malam = Color("#265979")
+const siang = Color("#ffffff")
+const selangWaktu = 0.00001
+
+var time = 0
+
+func _process(delta):
+	time += delta * 0.004
+	color = malam.lerp(siang,abs(sin(time)))
