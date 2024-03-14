1c3d1446e62dd4307a798ccdf95d06b88991abf9
icekimo49 <muhammad.haikal494972@gmail.com>
2024-03-14 23:55:16 +0700

    Ngilangin Penebang Spawner

--- scene/penebang_kayu_manager/penebang_kayu_manager.gd
+++ scene/penebang_kayu_manager/penebang_kayu_manager.gd
@@ -9,14 +9,14 @@ func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
 	
 	
-func on_timer_timeout():
-	var player = get_tree().get_first_node_in_group("player") as Node2D
-	if player == null :
-		return
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0,TAU))
-	var random_position = player.global_position + (random_direction * Spawn_Radius)
-	var enemy = penebang_kayu_scene.instantiate() as Node2D
-	get_parent().add_child(enemy)
-	enemy.global_position = random_position 
-	
+func on_timer_timeout(): #Buat ngespawn musuh
+	#var player = get_tree().get_first_node_in_group("player") as Node2D
+	#if player == null :
+		#return
+	#var random_direction = Vector2.RIGHT.rotated(randf_range(0,TAU))
+	#var random_position = player.global_position + (random_direction * Spawn_Radius)
+	#var enemy = penebang_kayu_scene.instantiate() as Node2D
+	#get_parent().add_child(enemy)
+	#enemy.global_position = random_position 
+	pass
 
