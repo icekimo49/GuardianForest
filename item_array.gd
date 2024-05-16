978fcf12d950c09dc365d57a664646d179eb2768
RayyanGanteng <hermantorayyan@gmail.com>
2024-05-16 15:11:38 +0700

    a

--- item_array.gd
+++ item_array.gd
@@ -1,4 +1,4 @@
 extends Resource
 
 class_name ItemArray
-@export var ItemPathArray: Array = ["res://script/inventory/items/batu.tres", ""]
+@export var ItemPathArray: Array = ["res://script/inventory/items/batu.tres", "res://script/inventory/items/GoldenApple.tres"]
