8870fae76a1e1e1c700783becfe3be03b096da3b
RayyanGanteng <hermantorayyan@gmail.com>
2024-05-28 02:12:09 +0700

    aa

---
+++ Sumber_air_transparan.gd
@@ -0,0 +1,15 @@
+extends Node2D
+var bisa_refill = true
+
+func _on_refill_body_entered(body):
+	if body.has_method("player"):
+		if bisa_refill:
+			GlobalScript.isi_air_gayung +=1
+			$cooldown_refill.start()
+			bisa_refill = false
+			print(GlobalScript.isi_air_gayung)
+
+
+func _on_cooldown_refill_timeout():
+	$cooldown_refill.stop()
+	bisa_refill = true
--- scene/detektor_pohon.gd
+++ scene/detektor_pohon.gd
@@ -6,7 +6,6 @@ func _ready():
 
 func _process(delta):
 	pass
-
 func _on_body_entered(body):
 	pass # Replace with function body.
 
